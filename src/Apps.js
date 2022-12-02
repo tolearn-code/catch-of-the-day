ba9c102911ae45bc15f0f5fae770900282ee5e64
tolearn-code <119284185+tolearn-code@users.noreply.github.com>
2022-12-02 19:41:36 +0530

    added css import

--- src/Apps.js
+++ src/Apps.js
@@ -1,6 +1,22 @@
 import React, { Fragment, Component } from 'react';
 import { render } from 'react-dom';
+import Header from './Header';
+import Inventory from './Inventory';
+import Order from './Order';
+import './css/style.css';
 
-class Apps extends Component {}
+class Apps extends Component {
+  render() {
+    return (
+      <Fragment>
+        <div>
+          <Header />
+        </div>
+        <Order />
+        <Inventory />
+      </Fragment>
+    );
+  }
+}
 
 export default Apps;
